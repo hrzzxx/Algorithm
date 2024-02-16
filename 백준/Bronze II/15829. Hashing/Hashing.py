b0953347319341c9479f4760c2ccd38423dd8270
hrzzxx <155941332+hrzzxx@users.noreply.github.com>
2024-02-16 19:55:17 +0900

    [Bronze II] Title: Hashing, Time: 60 ms, Memory: 31252 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze II/15829. Hashing/Hashing.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+eng_ = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z'.split(', ')
+eng = {eng_[i]: i+1 for i in range(len(eng_))}
+
+r = 31
+M = 1234567891
+l = int(input())
+s = input().rstrip()
+res = 0
+for i in range(l):
+    res += (eng[s[i]] * (r**i)) % M
+
+print(res)
