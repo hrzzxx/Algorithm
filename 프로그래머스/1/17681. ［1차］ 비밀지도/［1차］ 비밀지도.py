cc1a1c437463b46ee45abcf75bb25e23bd5f4150
hrzzxx <155941332+hrzzxx@users.noreply.github.com>
2024-04-09 17:22:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,15 +1,10 @@
-def convert(x, n):
-    x = bin(x)[2:]
-    if len(x) != n:
-        x = '0'*(n-len(x))+x
-    return x
 def solution(n, arr1, arr2):
     answer = []
     for a, b in zip(arr1, arr2):
-        a,b = convert(a,n), convert(b,n)
-        res = ''
-        for i,j in zip(a,b):
-            if int(i) or int(j): res += '#'
-            else: res += ' '
+        res = bin(a|b)[2:]
+        if len(res) != n:
+            res = '0'*(n-len(res))+res
+        res = ''.join(['#' if int(i) else ' ' for i in res])
         answer.append(res)
+        print(res)
     return answer
