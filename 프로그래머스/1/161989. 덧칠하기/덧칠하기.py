19a6b1c038189894f88faeac6ae4368fd342e5e1
hrzzxx <155941332+hrzzxx@users.noreply.github.com>
2024-04-11 17:50:20 +0900

    [level 1] Title: 덧칠하기, Time: 10.97 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,15 +1,13 @@
 def solution(n, m, section):
     answer = 0
-    li = [0]*(n+1)
-    tmp = []
     sec = set(section)
     
-    for i in section:
-        if i in sec:
-            cur = set(range(i,i+m))
-            # print(sec, cur, sec&cur)
-            if sec & cur: answer += 1
-            # answer += len(sec & cur)
+    # section만 순회하는 것이 핵심!
+    for i in section: # 칠해야하는 구역 순회
+        if i in sec: # 구역이 아직 칠해지지 않음
+            cur = set(range(i,i+m)) # 현재 구역부터 롤러 길이까지의 구간
+            # 현재 구간에서 칠해야하는 구역 번호 색칠 (칠해야 할 집합에서 삭제)
             sec -= cur
-            if not sec: break
+            answer += 1
+            if not sec: break # 더이상 칠할게 없으면 순회 중단
     return answer
